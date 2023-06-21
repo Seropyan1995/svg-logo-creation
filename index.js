da307631c7de114856250c5686285087a371b130
Gevork Seropyan <seropyangevork@rocketmail.com>
2023-06-21 11:58:54 -0400

    added file structures

---
+++ index.js
@@ -0,0 +1,6 @@
+// Import Inquirer module
+const inquirer = require("inquirer");
+// Imports File system module
+const fs = require("fs");
+// Imports classes from shapes.js file
+const Shape = require("./lib/shapes.js");
